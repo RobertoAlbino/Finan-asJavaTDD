bfce165fd1c5c0df18c039e9059f47663c4d587e
Roberto Albino Junior <junior_wash@hotmail.com>
2018-05-18 13:33:52 -0300

    Iniciando a estrutura principal do projeto

---
+++ src/main/com/esucri/financa/FinancaApp.java
@@ -0,0 +1,9 @@
+package main.com.esucri.financa;
+
+import main.com.esucri.financa.views.Menu;
+
+public class FinancaApp {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+    }    
+}
